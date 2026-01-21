8b7b7420c5bc72282d42b5ac91ba729adf0d0e41
turdle <shuruiguo1@gmail.com>
2026-01-21 18:37:38 +1100

    start menu + theming + dialogue
    - setting themes for buttons
    - animations upon hover for button + title text
    - (some of) npc dialogue script for a croissant

---
+++ e/startmenu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+@onready var animation = $AnimationPlayer
+
+func _on_area_2d_mouse_entered() -> void:
+	animation.play("text_pop")
+func _on_area_2d_mouse_exited() -> void:
+	animation.play("text_pop2")
+func _on_area_2d__mouse_entered() -> void:
+	animation.play("button_pop")
+func _on_area_2d__mouse_exited() -> void:
+	animation.play("button_pop2")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://e/get_order.tscn")
